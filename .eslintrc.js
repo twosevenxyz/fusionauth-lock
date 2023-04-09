60722af78f3d2614ac116ec0f1fe3c310fa48281
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2023-04-09 14:36:45 -0400

    Migrate to Vite

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,12 +12,10 @@ module.exports = {
     SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module'
+    parser: '@babel/eslint-parser'
   },
-  plugins: [
-    'vue'
-  ],
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
---
+++ vite.config.js
@@ -0,0 +1,48 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+import { defineConfig } from 'vite'
+import vue2 from '@vitejs/plugin-vue2'
+
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Components from 'unplugin-vue-components/vite'
+
+export function sharedConfig() {
+  return defineConfig({
+    plugins: [
+      vue2({
+        jsx: true
+      }),
+      Components({
+        resolvers: IconsResolver()
+      }),
+      Icons({ compiler: 'vue2' })
+    ],
+    resolve: {
+      alias: {
+        '~': fileURLToPath(new URL('./', import.meta.url)),
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
+  })
+}
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'fusionauth-lock.js'),
+      name: 'fusionauth-lock',
+      formats: ['cjs']
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
+  ...sharedConfig()
+})
